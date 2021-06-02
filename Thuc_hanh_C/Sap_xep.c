60ade56b290e983d7faf12296da938613ea39ba5
Do_Thanh <thanhdvtk@gmail.com>
2021-06-02 15:21:00 +0700

    no message

--- Thuc_hanh_C/Sap_xep.c
+++ Thuc_hanh_C/Sap_xep.c
@@ -1,81 +1,81 @@
-///*Bai toan sap xep so trong mang*/
-//#include <stdio.h>
-//#include <string.h>
-//
-//void nhap_mang(int mang[], int *sophantu);
-//void in_mang(int mang[], int sophantu);
-//void sap_xep_tang(int mang[], int sophantu);
-//void sap_xep_le (int mang[], int sophantu);
-//
-//int main()
-//{
-//    int mang[50];
-//    int x; // So phan tu cua mang
-//    printf("Nhap so phan tu cua mang:");
-//    scanf("%d", &x);
-//    nhap_mang(mang, &x);
-//    sap_xep_le(mang,x);
-//    in_mang(mang,x);
-//    sap_xep_tang(mang,x);
-//    in_mang(mang,x);
-//    return 0;
-//}
-//void nhap_mang(int mang[], int *sophantu)
-//{
-//    for(int i=0;i<*sophantu;i++)
-//    {
-//        printf("Phan tu thu %d: ", i);
-//        scanf("%d", &mang[i]);
-//    }
-//
-//}
-//void in_mang(int mang[], int sophantu)
-//{
-//    printf("\nCac phan tu cua mang: \n");
-//    for(int i=0;i<sophantu;i++)
-//    {
-//        printf("%d ",mang[i]);
-//    }
-//}
-//void sap_xep_tang(int mang[], int sophantu)
-//{
-//    int tg; // Bien trung gian de thuc hien doi gia tri 2 phan tu
-//    for(int i=0; i< sophantu-1;i++) // Dam bao cac vi tri deu duoc sap xep
-//    {
-//        for(int j=i+1; j<sophantu;j++) // Dam bao so sanh vi tri i voi tat ca cac vi tri khac trong mang
-//        {
-//            if (mang[i]>mang[j])
-//            {
-//                tg = mang[i];
-//                mang[i]=mang[j];
-//                mang[j]=tg;
-//            }
-//        }
-//    }
-//}
-//void sap_xep_le (int mang[], int sophantu)
-//{
-//    int tg;
-//    for(int i=0; i< sophantu-1;i++) // Dam bao cac vi tri deu duoc sap xep
-//    {
-//        if (mang[i]%2!=0)
-//        {
-//            for(int j=i+1; j<sophantu;j++) // Dam bao so sanh vi tri i voi tat ca cac vi tri khac trong mang
-//            {
-//
-//                if(mang[j]%2!=0) // Neu ca 2 vi tri i va j deu la so le thi tien hanh sap xep
-//                {
-//                    if (mang[i]>mang[j])
-//                    {
-//                        tg = mang[i];
-//                        mang[i]=mang[j];
-//                        mang[j]=tg;
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//
-//
+/*Bai toan sap xep so trong mang*/
+#include <stdio.h>
+#include <string.h>
+
+void nhap_mang(int mang[], int *sophantu);
+void in_mang(int mang[], int sophantu);
+void sap_xep_tang(int mang[], int sophantu);
+void sap_xep_le (int mang[], int sophantu);
+
+int main()
+{
+    int mang[50];
+    int x; // So phan tu cua mang
+    printf("Nhap so phan tu cua mang:");
+    scanf("%d", &x);
+    nhap_mang(mang, &x);
+    sap_xep_le(mang,x);
+    in_mang(mang,x);
+    sap_xep_tang(mang,x);
+    in_mang(mang,x);
+    return 0;
+}
+void nhap_mang(int mang[], int *sophantu)
+{
+    for(int i=0;i<*sophantu;i++)
+    {
+        printf("Phan tu thu %d: ", i);
+        scanf("%d", &mang[i]);
+    }
+
+}
+void in_mang(int mang[], int sophantu)
+{
+    printf("\nCac phan tu cua mang: \n");
+    for(int i=0;i<sophantu;i++)
+    {
+        printf("%d ",mang[i]);
+    }
+}
+void sap_xep_tang(int mang[], int sophantu)
+{
+    int tg; // Bien trung gian de thuc hien doi gia tri 2 phan tu
+    for(int i=0; i< sophantu-1;i++) // Dam bao cac vi tri deu duoc sap xep
+    {
+        for(int j=i+1; j<sophantu;j++) // Dam bao so sanh vi tri i voi tat ca cac vi tri khac trong mang
+        {
+            if (mang[i]>mang[j])
+            {
+                tg = mang[i];
+                mang[i]=mang[j];
+                mang[j]=tg;
+            }
+        }
+    }
+}
+void sap_xep_le (int mang[], int sophantu)
+{
+    int tg;
+    for(int i=0; i< sophantu-1;i++) // Dam bao cac vi tri deu duoc sap xep
+    {
+        if (mang[i]%2!=0)
+        {
+            for(int j=i+1; j<sophantu;j++) // Dam bao so sanh vi tri i voi tat ca cac vi tri khac trong mang
+            {
+
+                if(mang[j]%2!=0) // Neu ca 2 vi tri i va j deu la so le thi tien hanh sap xep
+                {
+                    if (mang[i]>mang[j])
+                    {
+                        tg = mang[i];
+                        mang[i]=mang[j];
+                        mang[j]=tg;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
--- Thuc_hanh_C/Thao_tac_voi_chuoi.c
+++ Thuc_hanh_C/Thao_tac_voi_chuoi.c
@@ -1,32 +1,32 @@
-///*Bai toan tach ngay thang nam trong chuoi*/
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//char mang[]="Today is 29/04/2021";
-//int main()
-//{
-//    char date[3];
-//    char month[3];
-//    char year[5];
-//    char year2[5];
-//    char *sub = strtok(mang, "/");
-//    sub = strtok(NULL, "/");
-//    strcpy(month,sub);
-//    // Xem vung dia chi va gia tri cua vung dia chi con tro sub tro toi
-//    printf("1: %x\n", sub);
-//    printf("2: %x\n",&mang[12]);
-//    printf("3: %s\n", sub);
-//
-//    strcpy(date,(sub-3));
-//    // Xem vung dia chi va gia tri cua vung dia chi con tro sub tro toi
-//    printf("1: %x\n", sub);
-//    printf("2: %x\n",&mang[9]);
-//    printf("3: %s\n", sub);
-//
-//    sub = strtok(NULL, "/");
-//    strcpy(year,sub);
-//
-//    printf(" %s - %s -%s", date, month, year);
-//    return 0;
-//}
+/*Bai toan tach ngay thang nam trong chuoi*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char mang[]="Today is 29/04/2021";
+int main()
+{
+    char date[3];
+    char month[3];
+    char year[5];
+    char year2[5];
+    char *sub = strtok(mang, "/");
+    sub = strtok(NULL, "/");
+    strcpy(month,sub);
+    // Xem vung dia chi va gia tri cua vung dia chi con tro sub tro toi
+    printf("1: %x\n", sub);
+    printf("2: %x\n",&mang[12]);
+    printf("3: %s\n", sub);
+
+    strcpy(date,(sub-3));
+    // Xem vung dia chi va gia tri cua vung dia chi con tro sub tro toi
+    printf("1: %x\n", sub);
+    printf("2: %x\n",&mang[9]);
+    printf("3: %s\n", sub);
+
+    sub = strtok(NULL, "/");
+    strcpy(year,sub);
+
+    printf(" %s - %s -%s", date, month, year);
+    return 0;
+}
